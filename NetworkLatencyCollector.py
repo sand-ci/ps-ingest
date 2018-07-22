26256bd20468bfb8fb9f2e3c09aa0875e4b6307e
Ilija Vukotic <ivukotic@cern.ch>
2018-07-22 14:22:44 -0500

    something is wrong in Latency data. now clearing it on errors.

--- NetworkLatencyCollector.py
+++ NetworkLatencyCollector.py
@@ -144,7 +144,10 @@ def eventCreator():
             succ = tools.bulk_index(aLotOfData, es_conn=es_conn, thread_name=threading.current_thread().name)
             if succ is True:
                 aLotOfData = []
-                
+            else:
+                print("ERROR - ", aLotOfData)
+                aLotOfData = []
+
         if len(aLotOfData) > 10000:
             print('too many entries in memory. sleep for a minute.')
             time.sleep(60)
